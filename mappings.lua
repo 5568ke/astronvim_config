00fedf8e157b0d5f844d4a147b351e702d56945e
5568ke <5568keb67595@gmail.com>
2023-06-29 23:18:25 +0800

    commit

--- mappings.lua
+++ mappings.lua
@@ -20,6 +20,8 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<c-l>"]={":bnext<cr>"},
+    ["<c-h>"]={":bprev<cr>"}
   },
   t = {
     -- setting a mapping to false will disable it
